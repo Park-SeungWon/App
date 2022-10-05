2be124ee51e61808fa095f48a7135b3687a74501
Park-SeungWon <68949530+Park-SeungWon@users.noreply.github.com>
2022-10-05 11:26:36 +0900

    rm cached

--- 코드/server/java/DataBase/Connection/ConnectionInfo.java
+++
@@ -1,8 +0,0 @@
-package DataBase.Connection;
-
-public class ConnectionInfo {
-    public  String url = "jdbc:mysql://localhost:3306/pingu";
-    public  String user = "root";
-    public  String password = "root";
-    public String driverName = "com.mysql.cj.jdbc.Driver";
-}
