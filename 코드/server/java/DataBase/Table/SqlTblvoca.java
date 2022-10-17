d4e666c35d6acba0f8cca417a42a160dfe488e3a
Park-SeungWon <68949530+Park-SeungWon@users.noreply.github.com>
2022-10-17 12:41:47 +0900

    jdbc

--- 코드/server/java/DataBase/Table/SqlTblvoca.java
+++ 코드/server/java/DataBase/Table/SqlTblvoca.java
@@ -1,9 +1,6 @@
 package DataBase.Table;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-
-
+import java.sql.*;
 import com.mysql.cj.jdbc.CallableStatement;
 
 import DataBase.Connection.ConnectionInfo;
@@ -16,15 +13,13 @@ public class SqlTblvoca {
         ConnectionInfo getconString = new ConnectionInfo();
 
         try {
-			Class.forName(getconString.driverName);
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
-			// ② 연결
-			connection = DriverManager.getConnection(getconString.url, getconString.user, getconString.password);
-
-
-            // insert
-			CallableStatement stat = (CallableStatement) connection.prepareCall("{CALL voca_Insert(?, ?, ?, ?, ?, ?, ?, ?)}");
-            stat.setString(1, null); //ID 고정
+            // ② 연결
+            connection = DriverManager.getConnection(getconString.url, getconString.user, getconString.password);
+            CallableStatement stat = (CallableStatement) connection
+                    .prepareCall("{CALL voca__UpdateNInsert(?, ?, ?, ?, ?, ?, ?, ?)}");
+            stat.setString(1, ""); // ID 고정
             stat.setString(2, "test");
             stat.setString(3, table.getMean());
             stat.setString(4, "명사");
@@ -34,10 +29,10 @@ public class SqlTblvoca {
             stat.setString(8, "127.0.0.1");
 
             stat.execute();
-		} catch (Exception e) {
-			System.out.println("[insert_user 오류]\n" + e.getLocalizedMessage());
-			return false;
-		}       
-      return true;
+        } catch (Exception e) {
+            System.out.println("[insert_user 오류]\n" + e.getLocalizedMessage());
+            return false;
+        } 
+        return true;
     }
 }
